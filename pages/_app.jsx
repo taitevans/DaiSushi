b4609ad65c1d625fe2c5526b2276bd0786a1fd94
taitevans <contact@taitevans.com>
2021-01-17 15:18:00 +1100

    Add document Head

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,17 @@
 import Navigation from "../components/Navigation";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => (
   <>
     <Navigation />
     <Component {...pageProps} />
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="description" content="Sushi made with Love!" />
+      <title>DaiSushi</title>
+    </Head>
 
     <style global jsx>{`
       * {
