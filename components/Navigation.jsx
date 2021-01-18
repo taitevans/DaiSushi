b4190c5f2e6e5ff325820e1f2568388fc9fb84ce
taitevans <contact@taitevans.com>
2021-01-18 14:43:52 +1100

    Add random colours to navigation drawer

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,70 +1,102 @@
 import Link from "next/link";
 
-const Navigation = (props) => (
-  <>
-    <nav
-      className={`${props.toggled ? "toggled" : ""} ${
-        props.drawer ? "drawer" : ""
-      }`}
-    >
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/page-2/">
-        <a>Page 2</a>
-      </Link>
-    </nav>
+let colours = ["pink", "green", "orange", "red"];
+let i = 0;
 
-    <style jsx>{`
-      nav {
-        display: none;
-      }
+export default function Navigation(props) {
+  if (props.toggled) {
+    if (i >= 3) {
+      i = 0;
+    } else {
+      i++;
+    }
+  }
 
-      .drawer {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        height: 100vh;
-        width: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        background-color: var(--bg);
-        pointer-events: none;
-        opacity: 0;
-        z-index: -3;
-        transition: opacity 0.2s ease-out;
-      }
+  return (
+    <>
+      <nav
+        className={`${props.toggled ? "toggled" : ""} ${
+          props.drawer ? "drawer" : ""
+        } ${colours[i]}`}
+      >
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/page-2/">
+          <a>Page 2</a>
+        </Link>
+      </nav>
 
-      .toggled {
-        opacity: 1;
-        pointer-events: auto;
-      }
-
-      a {
-        padding: 12px;
-      }
-
-      @media only screen and (min-width: 840px) {
+      <style jsx>{`
         nav {
-          display: flex;
-          z-index: 0;
-          opacity: 1;
-          flex-direction: row;
-          height: auto;
-          width: auto;
-          position: static;
-          pointer-events: auto;
-          transition: none;
+          display: none;
         }
 
         .drawer {
-          display: none;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
+          height: 100vh;
+          width: 100%;
+          position: absolute;
+          top: 0;
+          left: 0;
+          background-color: var(--bg);
+          pointer-events: none;
+          opacity: 0;
+          z-index: -3;
+          transition: opacity 0.2s ease-out;
         }
-      }
-    `}</style>
-  </>
-);
 
-export default Navigation;
+        .toggled {
+          opacity: 1;
+          pointer-events: auto;
+        }
+
+        .green {
+          background-color: var(--green);
+          color: var(--pink);
+        }
+
+        .orange {
+          background-color: var(--orange);
+          color: var(--red);
+        }
+
+        .red {
+          background-color: var(--red);
+          color: var(--orange);
+        }
+
+        .pink {
+          background-color: var(--pink);
+          color: var(--green);
+        }
+
+        a {
+          padding: 12px;
+          color: inherit;
+        }
+
+        @media only screen and (min-width: 840px) {
+          nav {
+            display: flex;
+            z-index: 0;
+            opacity: 1;
+            flex-direction: row;
+            height: auto;
+            width: auto;
+            position: static;
+            pointer-events: auto;
+            transition: none;
+          }
+
+          .drawer {
+            display: none;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
