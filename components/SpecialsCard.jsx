652a11a7810d3770af0b569d57bfb8bc35236785
taitevans <contact@taitevans.com>
2021-01-26 11:36:31 +1100

    Add SpecialsCard mobile styles

--- components/SpecialsCard.jsx
+++ components/SpecialsCard.jsx
@@ -1,17 +1,25 @@
 const SpecialsCard = (props) => (
-  <div className="specials-card">
-    <picture>
-      <source type="image/webp" srcSet={props.webp} />
-      <img src={props.img} alt={props.alt} />
-    </picture>
-    <h1>{props.title}</h1>
-  </div>
-);
+  <>
+    <div className="specials-card">
+      <picture>
+        <source type="image/webp" srcSet={props.webp} />
+        <img src={props.img} alt={props.alt} />
+      </picture>
+      <h1>{props.title}</h1>
+    </div>
 
-<style jsx>{`
-  h1 {
-    text-transform: uppercase;
-  }
-`}</style>;
+    <style jsx>{`
+      .specials-card {
+        padding: 32px 0;
+      }
+
+      h1 {
+        text-transform: uppercase;
+        width: 160px;
+        line-break: anywhere;
+      }
+    `}</style>
+  </>
+);
 
 export default SpecialsCard;
