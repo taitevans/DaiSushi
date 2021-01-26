2ddd18977c2bd7bccba0a870114930a0e31ede7a
taitevans <contact@taitevans.com>
2021-01-26 11:44:32 +1100

    Add SpecialsCard text colours

--- components/SpecialsCard.jsx
+++ components/SpecialsCard.jsx
@@ -1,6 +1,6 @@
 const SpecialsCard = (props) => (
   <>
-    <div className="specials-card">
+    <div className={"specials-card " + props.title}>
       <picture>
         <source type="image/webp" srcSet={props.webp} />
         <img src={props.img} alt={props.alt} />
@@ -17,6 +17,22 @@ const SpecialsCard = (props) => (
         text-transform: uppercase;
         width: 160px;
         line-break: anywhere;
+      }
+
+      .luvyou {
+        color: var(--green);
+      }
+
+      .nigiri {
+        color: var(--red);
+      }
+
+      .uramaki {
+        color: var(--pink);
+      }
+
+      .maki {
+        color: var(--orange);
       }
     `}</style>
   </>
