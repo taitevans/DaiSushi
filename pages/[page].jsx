d3be19a94b099ec6d6711191a3b88ead7d19ea05
taitevans <contact@taitevans.com>
2021-01-28 12:49:27 +1100

    Add dynamic page route

---
+++ pages/[page].jsx
@@ -0,0 +1,51 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Head from "next/head";
+import marked from "marked";
+
+const Page = ({ htmlString, data }) => (
+  <>
+    <Head>
+      <title>{data.title}</title>
+      <meta title="description" content={data.description} />
+    </Head>
+    <h1>{data.title}</h1>
+    <div dangerouslySetInnerHTML={{ __html: htmlString }} />
+  </>
+);
+
+export const getStaticPaths = async () => {
+  const files = fs.readdirSync("content");
+  console.log("files: ", files);
+  const paths = files.map((filename) => ({
+    params: {
+      page: filename.replace(".md", ""),
+    },
+  }));
+  console.log("paths: ", paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { page } }) => {
+  const markdownWithMetadata = fs
+    .readFileSync(path.join("content", page + ".md"))
+    .toString();
+
+  const parsedMarkdown = matter(markdownWithMetadata);
+
+  const htmlString = marked(parsedMarkdown.content);
+
+  return {
+    props: {
+      htmlString,
+      data: parsedMarkdown.data,
+    },
+  };
+};
+
+export default Page;
