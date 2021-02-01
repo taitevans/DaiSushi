abd270cb422088098b68d0887bb47ae937e3ca2d
taitevans <contact@taitevans.com>
2021-02-01 16:42:20 +1100

    Add table parsing to page route

--- pages/[page].jsx
+++ pages/[page].jsx
@@ -4,16 +4,28 @@ import matter from "gray-matter";
 import Head from "next/head";
 import marked from "marked";
 
-const Page = ({ htmlString, data }) => (
-  <>
-    <Head>
-      <title>{data.title}</title>
-      <meta title="description" content={data.description} />
-    </Head>
-    <h1>{data.title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: htmlString }} />
-  </>
-);
+export default function Page({ htmlString, data, rawData }) {
+  let item = data.table
+    ? require("mdtable2json").getTables(rawData)[0].json
+    : null;
+  let itemKeys = data.table ? Object.keys(item[0]) : null;
+  return (
+    <>
+      <Head>
+        <title>{data.title}</title>
+        <meta title="description" content={data.description} />
+      </Head>
+      <h1>{data.title}</h1>
+      {data.table === true &&
+        Object.keys(item).map((key, i) => (
+          <div key={i}>{item[key][itemKeys[0]]}</div>
+        ))}
+      {data.table === false && (
+        <div dangerouslySetInnerHTML={{ __html: htmlString }} />
+      )}
+    </>
+  );
+}
 
 export const getStaticPaths = async () => {
   const files = fs.readdirSync("content");
@@ -47,5 +59,3 @@ export const getStaticProps = async ({ params: { page } }) => {
     },
   };
 };
-
-export default Page;
