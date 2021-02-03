6e1d4e504bbf5ffe92d4ada31c5138ce5fd70fdd
taitevans <contact@taitevans.com>
2021-02-03 14:19:22 +1100

    Add DoorDash logo

---
+++ components/DoorDashLogo.jsx
@@ -0,0 +1,15 @@
+const DoorDashLogo = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 128 128"
+    height="128"
+  >
+    <path
+      fill="#FF3008"
+      d="M123 44.8a32.5 32.5 0 00-28.7-17.2H3A3.1 3.1 0 001 33l19.9 20a9.3 9.3 0 006.6 2.7h64.3a8.3 8.3 0 01.1 16.6H47.5a3.1 3.1 0 00-2.2 5.3l19.9 20a9.3 9.3 0 006.6 2.8h20c26.1 0 45.8-27.9 31.2-55.6"
+    />
+  </svg>
+);
+
+export default DoorDashLogo;
