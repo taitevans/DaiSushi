9c89ee154ed514cf8514e95c756c73f5609b720e
taitevans <contact@taitevans.com>
2021-02-08 13:52:50 +1100

    Fix star icon height

--- components/HalfStarIcon.jsx
+++ components/HalfStarIcon.jsx
@@ -1,9 +1,13 @@
 const HalfStarIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 16 16"
+    height="16"
+  >
     <path
       fill="#000"
       d="M8 1.33L6.13 5.75l-4.8.41 3.64 3.15L3.88 14 8 11.51V1.33z"
-      height="16"
     />
   </svg>
 );
--- components/StarIcon.jsx
+++ components/StarIcon.jsx
@@ -1,9 +1,13 @@
 const StarIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 16 16"
+    height="16"
+  >
     <path
       fill="#000"
       d="M8 11.513L12.12 14l-1.093-4.687 3.64-3.153-4.794-.413L8 1.333 6.127 5.747l-4.794.413 3.634 3.153L3.88 14 8 11.513z"
-      height="16"
     />
   </svg>
 );
