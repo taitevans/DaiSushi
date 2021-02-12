53999344be6fbf0ccf0b44653defb5c1596e75df
taitevans <contact@taitevans.com>
2021-02-12 16:36:53 +1100

    Add phone number link/price to ListCard

--- components/ListCard.jsx
+++ components/ListCard.jsx
@@ -2,7 +2,15 @@ const ListCard = (props) => (
   <>
     <h3>{props.heading}</h3>
     <p>{props.f1}</p>
-    <p>{props.f2}</p>
+
+    {/* Phone number */}
+    {props.f2.slice(0, 3) === "+61" && (
+      <a href={`tel:${props.f2.replace(/\s/g, "")}`}>{props.f2}</a>
+    )}
+
+    {/* Price */}
+    {props.f2.slice(0, 3) !== "+61" && <p>${props.f2}</p>}
+
     <img src={props.image} alt="Image" />
   </>
 );
