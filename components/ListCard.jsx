3aff0df65ed4ea60b5748f87084648a1570eeb4c
taitevans <contact@taitevans.com>
2021-02-15 15:23:25 +1100

    Add ListCard styles

--- components/ListCard.jsx
+++ components/ListCard.jsx
@@ -1,17 +1,73 @@
 const ListCard = (props) => (
   <>
-    <h3>{props.heading}</h3>
-    <p>{props.f1}</p>
+    <div className="card">
+      <h4>{props.heading}</h4>
+      <p>{props.f1}</p>
 
-    {/* Phone number */}
-    {props.f2.slice(0, 3) === "+61" && (
-      <a href={`tel:${props.f2.replace(/\s/g, "")}`}>{props.f2}</a>
-    )}
+      {/* Phone number */}
+      {props.f2.slice(0, 3) === "+61" && (
+        <a href={`tel:${props.f2.replace(/\s/g, "")}`}>
+          <h6>{props.f2}</h6>
+        </a>
+      )}
 
-    {/* Price */}
-    {props.f2.slice(0, 3) !== "+61" && <p>${props.f2}</p>}
+      {/* Price */}
+      {props.f2.slice(0, 3) !== "+61" && <h6>${props.f2}</h6>}
+    </div>
+    <div className="image-container">
+      <img src={`${props.image}.jpg`} alt="Image" />
+    </div>
 
-    <img src={props.image} alt="Image" />
+    <style jsx>{`
+      .card {
+        padding: 16px;
+        background-color: var(--red);
+        grid-column: span 2;
+        border-radius: 24px 0 0 24px;
+        margin-right: -16px;
+        display: flex;
+        flex-direction: column;
+      }
+
+      h4 {
+        margin-bottom: 6px;
+      }
+
+      p:nth-child(2) {
+        margin-bottom: auto;
+        color: var(--text-2);
+      }
+
+      a:nth-child(3),
+      p:nth-child(3) {
+        margin-top: auto;
+      }
+
+      .image-container {
+        grid-column: span 2;
+        overflow: hidden;
+        border-radius: 24px;
+        margin: -24px 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      img {
+        height: 248px;
+      }
+
+      @media only screen and (min-width: 840px) {
+        .card {
+          grid-column: span 3;
+          margin-right: -24px;
+        }
+
+        .image-container {
+          grid-column: span 3;
+        }
+      }
+    `}</style>
   </>
 );
 
